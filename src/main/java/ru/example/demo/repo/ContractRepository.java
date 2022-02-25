af383cd2722035ca2a898ac6f7c42bad99e79a41
Dyatlov_Yuriy <dyk97@yandex.ru>
2022-02-25 20:16:07 +0300

    Fifth commit

---
+++ src/main/java/ru/example/demo/repo/ContractRepository.java
@@ -0,0 +1,9 @@
+package ru.example.demo.repo;
+
+import ru.example.demo.model.Contract;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContractRepository extends JpaRepository<Contract, String> {
+}
---
+++ src/main/java/ru/example/demo/repo/OptionRepository.java
@@ -0,0 +1,9 @@
+package ru.example.demo.repo;
+
+import ru.example.demo.model.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OptionRepository extends JpaRepository <Option, Integer> {
+}
